6bb6765b079f9812bb670f24ed4dbcb115dc273a
hunain12iqbal <hunnayyt@gmail.com>
2022-10-24 15:17:15 +0500

    app upload
    progress bar

---
+++ customize_variant_steps/static/src/js/main_mixin.js
@@ -0,0 +1,55 @@
+odoo.define('customize_variant_steps.main_', function (require) {
+    "use strict";
+
+    var concurrency = require('web.concurrency');
+    var core = require('web.core');
+    var utils = require('web.utils');
+    var ajax = require('web.ajax');
+    var _t = core._t;
+
+    
+    console.log("js file loaded")
+    
+    var navListItems = $('div.setup-panel div a'),
+            allWells = $('.setup-content'),
+            allNextBtn = $('.nextBtn');
+  
+    allWells.hide();
+  
+    navListItems.click(function (e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+                $item = $(this);
+  
+        if (!$item.hasClass('disabled')) {
+            navListItems.removeClass('btn-primary').addClass('btn-default');
+            $item.addClass('btn-primary');
+            allWells.hide();
+            $target.show();
+            $target.find('input:eq(0)').focus();
+        }
+    });
+  
+    allNextBtn.click(function(){
+        var curStep = $(this).closest(".setup-content"),
+            curStepBtn = curStep.attr("id"),
+            nextStepWizard = $('div.setup-panel div a[href="#' + curStepBtn + '"]').parent().next().children("a"),
+            curInputs = curStep.find("input[type='text'],input[type='url']"),
+            isValid = true;
+  
+        $(".form-group").removeClass("has-error");
+        for(var i=0; i<curInputs.length; i++){
+            if (!curInputs[i].validity.valid){
+                isValid = false;
+                $(curInputs[i]).closest(".form-group").addClass("has-error");
+            }
+        }
+  
+        if (isValid)
+            nextStepWizard.removeAttr('disabled').trigger('click');
+    });
+  
+    $('div.setup-panel div a.btn-primary').trigger('click');
+    
+  });
+  
